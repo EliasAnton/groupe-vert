6fffcd6a108fe5ee94645e33d0fa3f9f0b06b616
LeoLumpi <lumpi@imt.com>
2021-12-14 13:45:31 +0100

    still trying to get the robot move

--- leo_test/scripts/move-1-meter.py
+++ leo_test/scripts/move-1-meter.py
@@ -10,11 +10,8 @@ commandPublisher = rospy.Publisher(
     Twist, queue_size=10
 )
 
-# :
+# Publish velocity commandes:
 def move_command(data):
-    #pub = rospy.Publisher('turtle1/cmd_vel', Twist , queue_size=10)
-    #rospy.init_node('move-1-meter', anonymous=True)
-    #rate = rospy.Rate(0.1) # 0.1 hz
     # Compute cmd_vel here and publish... (do not forget to reduce timer duration)
     cmd= Twist()
     cmd.linear.x= 0.1
@@ -24,6 +21,5 @@ def move_command(data):
 rospy.Timer( rospy.Duration(0.1), move_command, oneshot=False )
 
 # spin() enter the program in a infinite loop
-
 print("Start move.py")
 rospy.spin()
