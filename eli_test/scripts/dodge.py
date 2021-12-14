4c7f2e73b6f243df63129c50d41cf3e9eafc8a85
EliasAnton <el@ant.com>
2021-12-14 13:54:48 +0100

    bagfile data can be read

---
+++ eli_test/scripts/dodge.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import rospy, math
+from sensor_msgs.msg import LaserScan
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I heard %s")
+    obstacles= []
+    angle= data.angle_min
+    for aDistance in data.ranges :
+        if 0.1 < aDistance and aDistance < 5.0 :
+            aPoint= [ 
+                math.cos(angle) * aDistance, 
+                math.sin( angle ) * aDistance
+            ]
+            obstacles.append( aPoint )
+        angle+= data.angle_increment
+    rospy.loginfo( str(
+        [ [ round(p[0], 2), round(p[1], 2) ] for p in  obstacles[0:10] ] 
+    ) + " ..." )
+    
+def listener():
+
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    rospy.init_node('dodge', anonymous=True)
+
+    rospy.Subscriber("scan", LaserScan, callback)
+
+    # spin() simply keeps python from exiting until this node is stopped
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
