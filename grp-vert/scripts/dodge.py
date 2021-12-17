772e89866c06cddcb1c9048cd2c564457488e061
j <j@s.com>
2021-12-17 13:02:16 +0100

    working version of real robot

--- grp-vert/scripts/dodge.py
+++ grp-vert/scripts/dodge.py
@@ -1,30 +1,16 @@
 #!/usr/bin/env python3
-import rospy, math, std_msgs.msg
+import rospy, math, std_msgs.msg, time
 from sensor_msgs.msg import LaserScan
-from sensor_msgs.msg import PointCloud
-from geometry_msgs.msg import Point32, Twist
+from geometry_msgs.msg import Twist
 
-obstacles= []
-laserdataPublisher = rospy.Publisher(
-    'poinCloud_from_scanner',
-    PointCloud, queue_size=10
-)
+rospy.init_node('dodge', anonymous=True)
+
 commandPublisher = rospy.Publisher(
     '/cmd_vel_mux/input/navi',
     Twist, queue_size=10
 )
 
 def main():
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('dodge', anonymous=True)
-
-    # call the scan_data at a regular frequency:
-    #rospy.Timer( rospy.Duration(0.1), publish_scan_data, oneshot=False )
-    rospy.sleep(0.5)
     #rospy.Subscriber("scan", LaserScan, callback)
     rospy.Subscriber("scan", LaserScan, move_command)
 
@@ -33,57 +19,37 @@ def main():
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
-
-def callback(data):
-    global obstacles
-    angle= data.angle_min
-    for aDistance in data.ranges :
-        if 0.1 < aDistance and aDistance < 5.0 :
-            aPoint= [ 
-                math.cos(angle) * aDistance, 
-                math.sin( angle ) * aDistance,
-                0.0
-            ]
-            obstacles.append( aPoint )
-        angle+= data.angle_increment
-    rospy.loginfo( str(
-        [ [ round(p[0], 2), round(p[1], 2) ] for p in  obstacles[0:10] ] 
-    ) + " ..." )
-    publish_scan_data()
-    
-
-# Publish scan data:
-def publish_scan_data():
-    global obstacles
-    global laserdataPublisher
-    pc = PointCloud()
-    header = std_msgs.msg.Header()
-    header.frame_id = "laser"
-    header.stamp = rospy.Time.now()
-    header.frame_id = 'map'
-    pc.header = header
-    #for p in obstacles :
-    #    pc.points.append(p)
-    #pc.points.append(obstacles)
-    pc.points.append(Point32(1.0, 1.0, 0.0))
-    laserdataPublisher.publish(pc)
-
 def move_command(data):
-    # Compute cmd_vel here and publish... (do not forget to reduce timer duration)
     cmd= Twist()
+
     angle= data.angle_min
-    for aDistance in data.ranges :
-        if 0.1 < aDistance and aDistance < 5.0:
-            aPoint= [ 
-                math.cos(angle) * aDistance, 
-                math.sin(angle) * aDistance,
-                0.0
-            ]
-        if aPoint[0] < 1:
-            cmd.linear.y= 0.5
-        else:
-            cmd.linear.x= 0.5
-    commandPublisher.publish(cmd)
+    angles= []
+    for a in data.ranges :
+        angles.append(angle)
+        angle+= data.angle_increment
+    
+    middle = len(angles) / 2
+    middle = int(middle)
+    quarter = len(angles) / 4
+    quarter = int(quarter)
+    eights = len(angles) / 8
+    eights = int(eights)
+
+    close = 0
+    
+    for b in data.ranges[middle - quarter - 30: middle + eights + 30]:
+        if b < 0.4:
+            close = close + 1
+
+    #turn, if most of the sensors in the important angle report an obstacle close by
+    if close > quarter / 3:
+        cmd.linear.x = 0
+        cmd.angular.z= 0.8
+        commandPublisher.publish(cmd)
+    else:
+        cmd.linear.x= 0.5
+        cmd.angular.z= 0
+        commandPublisher.publish(cmd)
 
 if __name__ == '__main__':
     main()
