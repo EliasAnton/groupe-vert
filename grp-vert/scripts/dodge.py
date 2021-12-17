90f70682e6d447097674b327d777f4be3f57345d
EliasAnton <el@ant.com>
2021-12-17 15:23:26 +0100

    added comments to scripts

--- grp-vert/scripts/dodge.py
+++ grp-vert/scripts/dodge.py
@@ -17,24 +17,28 @@ def main():
 def move_command(data):
     cmd= Twist()
 
+    #saves all the angles in a list. The list of angles corresponds to the list of ranges
     angle= data.angle_min
     angles= []
     for a in data.ranges :
         angles.append(angle)
         angle+= data.angle_increment
     
+    #middle is the angle value for the position directly in front of the robot
     middle = len(angles) / 2
     middle = int(middle)
     quarter = len(angles) / 4
     quarter = int(quarter)
 
+    #we count all the individual laser rays in a certain angle in front of the robot, which report an obstacle closer than 0,4
     close = 0
-    
     for b in data.ranges[middle - quarter - 30: middle + quarter]:
         if b < 0.4:
             close = close + 1
 
     #turn, if most of the sensors in the important angle report an obstacle close by
+    #we did this, because the sensor probably picked up some false readings and the robot startet to shake
+    #with this solution, some false readings don't interrupt a smooth turn
     if close > quarter / 3:
         cmd.linear.x = 0
         cmd.angular.z= 0.8
--- grp-vert/scripts/dodge_for_sim.py
+++ grp-vert/scripts/dodge_for_sim.py
@@ -6,48 +6,45 @@ from geometry_msgs.msg import Twist
 rospy.init_node('dodge', anonymous=True)
 
 commandPublisher = rospy.Publisher(
-    '/cmd_vel_mux/input/navi',
+    '/cmd_vel',
     Twist, queue_size=10
 )
 
 def main():
-    #rospy.Subscriber("scan", LaserScan, callback)
     rospy.Subscriber("scan", LaserScan, move_command)
-
-    #rospy.Timer( rospy.Duration(0.1), move_command, oneshot=False )
-
-    # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 def move_command(data):
     cmd= Twist()
 
+    #saves all the angles in a list. The list of angles corresponds to the list of ranges
     angle= data.angle_min
     angles= []
     for a in data.ranges :
         angles.append(angle)
         angle+= data.angle_increment
     
+    #middle is the angle value for the position directly in front of the robot
     middle = len(angles) / 2
     middle = int(middle)
     quarter = len(angles) / 4
     quarter = int(quarter)
-    eights = len(angles) / 8
-    eights = int(eights)
 
+    #we count all the individual laser rays in a certain angle in front of the robot, which report an obstacle closer than 0,4
     close = 0
-    
-    for b in data.ranges[middle - quarter - 30: middle + eights + 30]:
-        if b < 0.4:
+    for b in data.ranges[middle - quarter - 30: middle + quarter]:
+        if b < 1:
             close = close + 1
 
     #turn, if most of the sensors in the important angle report an obstacle close by
+    #we did this, because the sensor probably picked up some false readings and the robot startet to shake
+    #with this solution, some false readings don't interrupt a smooth turn
     if close > quarter / 3:
         cmd.linear.x = 0
         cmd.angular.z= 0.8
         commandPublisher.publish(cmd)
     else:
-        cmd.linear.x= 0.5
+        cmd.linear.x= 2
         cmd.angular.z= 0
         commandPublisher.publish(cmd)
 
